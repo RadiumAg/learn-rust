4a2ca16e247ce74abdec1fdc1bfe6c090ae391ba
Radium <2043869032@qq.com>
2022-09-03 21:17:01 +0800

    init🥳

---
+++ target/debug/deps/learn_rust-89bca3ede4fb58fe.d
@@ -0,0 +1,5 @@
+d:\work\learn-rust\target\debug\deps\learn_rust-89bca3ede4fb58fe.rmeta: src\main.rs
+
+d:\work\learn-rust\target\debug\deps\learn_rust-89bca3ede4fb58fe.d: src\main.rs
+
+src\main.rs:
